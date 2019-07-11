e839effd3c8e89bd38c42d2711a5fc64490f1c24
Dany Hakim <dany_hakim@yahoo.com>
2019-07-11 15:20:48 +0700

    Tap and Gesture

--- TapGestureHandsOn/ViewController.swift
+++ TapGestureHandsOn/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rectangleView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func tapped(_ sender: UITapGestureRecognizer) {
+        rectangleView.backgroundColor = #colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1)
+    }
+    
+    
+    @IBAction func panned(_ sender: UIPanGestureRecognizer) {
+        //mengambil view
+        guard let recognizerView = sender.view else {return}
+        //mengambil translasi
+        let translasi = sender.translation(in: view)
+        //mentranslasikan view
+        recognizerView.center.x += translasi.x
+        recognizerView.center.y += translasi.y
+        //memunculkannya di aplikasi
+        sender.setTranslation(.zero, in: view)
+    }
+    
+    
 }
 
