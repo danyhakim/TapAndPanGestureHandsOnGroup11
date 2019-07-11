329408b4d8fd4a4d45872f999ebaeaf8f7fd5311
Dany Hakim <dany_hakim@yahoo.com>
2019-07-11 14:17:04 +0700

    Initial Commit

---
+++ TapGestureHandsOn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapGestureHandsOn
+//
+//  Created by Dany Hakim on 11/07/19.
+//  Copyright © 2019 Dany Hakim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
